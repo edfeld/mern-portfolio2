d1fc7bce082fa22160c62e67841370d59134b5a7
edfeld <edfeld1@gmail.com>
2019-04-30 15:06:28 -0700

    Fill in JSON, add fields to project, pixel fixes

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,35 @@
 import React from "react"
 
+// function Project (props) {
+//     return (
+//         <a key={props.projectItem.id} href={props.projectItem.projectLink} target="_blank" rel="noopener noreferrer">
+//             <figure className="figure ml-2 mb-0">
+//                 <img style={{height: 170}} src={props.projectItem.image} className="figure-img img-fluid rounded border border-secondary" alt={props.projectItem.altText}/>
+//                 <figcaption className="figure-caption text-center position-relative py-2">{props.projectItem.name}</figcaption>
+//             </figure>
+//         </a>
+//     )
+// }
+
+// export default Project
+
 function Project (props) {
     return (
-        <a key={props.projectItem.id} href={props.projectItem.projectLink} target="_blank" rel="noopener noreferrer">
-            <figure className="figure ml-2 mb-0">
-                <img style={{height: 170}} src={props.projectItem.image} className="figure-img img-fluid rounded border border-secondary" alt={props.projectItem.altText}/>
-                <figcaption className="figure-caption text-center position-relative py-2">{props.projectItem.name}</figcaption>
-            </figure>
-        </a>
-    )
+        <div key={props.projectItem.id} className="project card border-secondary rounded mb-2 mx-2 float-left">
+            <div className="container rounded-top bg-light px-2"><img class="card-img-top rounded-0"
+                    src={props.projectItem.image} alt={props.projectItem.altText}></img>
+            </div>
+            <div className="card-body border-top">
+                <h5 className="card-title">{props.projectItem.name}</h5>
+                <p className="card-text">{props.projectItem.summary}.</p>
+            </div>
+            <div className="card-footer clearfix">
+                <a href={props.projectItem.projectLink} className="card-link" target="_blank" rel="noopener noreferrer"><i class="fas fa-code"></i> Project Link</a>
+
+                <a href={props.projectItem.githubLink} className="card-link float-right" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i> GitHub</a>
+            </div>
+        </div>
+        )
 }
 
 export default Project
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,24 +4,33 @@ import Project from "../components/Project"
 
 function Portfolio () {
 
-    const projectsMap = Projects.map(project => {
+    const ProjectsFilter = Projects.filter( project => { return !project.isHide})
+    const projectsMap = ProjectsFilter.map(project => {
         return (
         <Project projectItem={project} key={project.id} />
         )
     })
+    // const projectsMap = Projects.filter(function (project) {
+    //     if (project.isHide === 0) 
+    //     return (
+    //     <Project projectItem={project} key={project.id} />
+    //     )
+    
+    // })
+
     return (
         <div>
-            <div className="container main-container col-12 col-md-10 offset-md-1 clear-fix mt-3">
-            <div className="row">
-                <div className="col-md-12 col-12  m-2">
-                    <h1 className="border-bottom border-secondary" >Portfolio</h1>
+            <div className="container main-container col-12 col-md-10 offset-md-1 clearfix mt-3">
+                <div className="row">
+                    <div className="col-md-12 col-12  m-2">
+                        <h1 className="border-bottom border-secondary" >Portfolio</h1>
+                    </div>
+                </div>    
+                <div className="row port-img-row ml-auto mr-auto">
+                    <div>
+                        {projectsMap}
+                    </div>
                 </div>
-            </div>    
-            <div className="row port-img-row  ml-auto mr-auto">
-                <div>
-                    {projectsMap}
-                </div>
-            </div>
             </div>
         </div>
     )
